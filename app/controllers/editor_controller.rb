4ecf7fb84224c9c787b1aedb04f8519b3148d8c8
Takeshi Nakamura <taqueci.n@gmail.com>
2019-10-18 22:56:00 +0900

    Code cleanup

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -1,3 +1,4 @@
+# Editor controller
 class EditorController < ApplicationController
   TERM_LEN_PUB_PROJ_MIN = 2
   AUTOCOMPLETE_NUM_MAX = 10
@@ -12,34 +13,35 @@ class EditorController < ApplicationController
 
     raise unless authorized?(proj)
 
-    scope = (proj.is_public && (term.length >= TERM_LEN_PUB_PROJ_MIN)) ?
-              User.all : proj.users
+    is_public = (proj.is_public && (term.length >= TERM_LEN_PUB_PROJ_MIN))
+    scope = is_public ? User.all : proj.users
 
     users = scope.active.visible.sorted.like(term)
-              .where.not(id: params[:exclude])
-              .limit(AUTOCOMPLETE_NUM_MAX).to_a
+                 .where.not(id: params[:exclude])
+                 .limit(AUTOCOMPLETE_NUM_MAX).to_a
 
-    render json: users.map {|user|
+    render json: users.map { |user|
       {
         label: user.name.to_s,
         id: user.id.to_s,
         avatar: avatar_image_tag(user)
       }
     }
-  rescue
+  rescue StandardError
     render_error status: 403
   end
 
   def authorized?(project)
-    User.current.allowed_to?({controller: 'projects', action: 'show'}, project)
+    User.current.allowed_to?({ controller: 'projects', action: 'show' },
+                             project)
   end
 
   def avatar_image_tag(user)
     avtr = view_context.avatar(user, size: AVATAR_SIZE.to_s)
 
-    avtr.blank? ?
-      view_context.image_tag('default.png', plugin: 'redmine_wysiwyg_editor',
-                             size: AVATAR_SIZE.to_s, class: 'gravatar') :
-      avtr
+    avtr.presence ||
+      view_context.image_tag('default.png',
+                             plugin: 'redmine_wysiwyg_editor',
+                             size: AVATAR_SIZE.to_s, class: 'gravatar')
   end
 end
--- lib/redmine_wysiwyg_editor.rb
+++ lib/redmine_wysiwyg_editor.rb
@@ -1,3 +1,5 @@
+# Redmine WYSIWYG Editor hook listener
 class RedmineWysiwygEditorHookListener < Redmine::Hook::ViewListener
-  render_on :view_layouts_base_html_head, :partial => "redmine_wysiwyg_editor/redmine_wysiwyg_editor_partial"
+  render_on :view_layouts_base_html_head,
+            partial: 'redmine_wysiwyg_editor/redmine_wysiwyg_editor_partial'
 end
