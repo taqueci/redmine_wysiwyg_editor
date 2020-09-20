f8e7ebf7877470f6454af98aa5da93481425e5d3
Takeshi Nakamura <taqueci.n@gmail.com>
2020-09-20 11:51:26 +0900

    Bump version

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_wysiwyg_editor do
   name 'Redmine WYSIWYG Editor plugin'
   author 'Takeshi Nakamura'
   description 'Redmine WYSIWYG text editor'
-  version '0.16.4'
+  version '0.17.0'
   url 'https://github.com/taqueci/redmine_wysiwyg_editor'
   author_url 'https://github.com/taqueci'
 end
